92d5479529042c81dd3e7f512ebf4c82f1a0f6a4
xing24xing <khushipandey260304@gmail.com>
2024-08-04 02:57:42 +0530

    Added More Function In My LinkedList Curd Operations

--- Linked List/Curd.py
+++ Linked List/Curd.py
@@ -26,22 +26,61 @@ class LinkedList:
            self.head = Node(data,None)
            return
        temp = self.head
-       while temp:
+       while temp.next:
            temp = temp.next
        temp.next = Node(data,None)
-    def insert_Values(self,data_list):
-        self.head = None
 
+    def insert_Values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+    def get_len(self):
+        c = 0
+        t = self.head
+        while t:
+            c += 1
+            t = t.next
+        return c
+    def remove(self,index):
+        if index < 0 or index >= self.get_len():
+            raise Exception("Invalid Index")
+        if index == 0:
+           self.head = self.head.next
+        t = self.head
+        c = 0
+        while t:
+            if c == index - 1:
+                t.next = t.next.next
+                break
+            c += 1
+            t = t.next
+
+    def insert_At(self,index,data):
+        if index < 0 or index >= self.get_len():
+            raise Exception("Invalid Index")
+
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        t = self.head
+        c = 0
+        while t:
+            if c == index - 1:
+                n = Node(data,t.next)
+                t.next = n
+                break
+            c += 1
+            t = t.next
 
 if __name__ == '__main__':
      l = LinkedList()
-     # l.insert_at_bregining(23)
-     # l.insert_at_bregining(45)
-     l.insert_at_end(45)
-     l.insert_at_begining(89)
-     # l.insert_at_end(3455)
-     l.insert_at_begining(56)
-     l.insert_at_begining(89)
+     l.insert_Values(['mango','banana','papaya'])
      l.print()
+     print("length ", l.get_len())
+     # l.remove(1)
+     # l.print()
+     l.insert_At(0,"banana")
+     l.print()
+     print("length ",l.get_len())
 
 
